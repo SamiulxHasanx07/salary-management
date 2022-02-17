bf121194b2644d5efd8d1928139365639f186494
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2022-02-17 12:06:08 +0600

    input value from function

--- js/app.js
+++ js/app.js
@@ -1,61 +1,88 @@
+// Get Input Value & Convert Number 
+function valueToNumber(inputId){
+    // get total Income 
+    const inputField = document.getElementById(inputId);
+    const inputFieldValue = inputField.value;
+    const inputValue = parseFloat(inputFieldValue)
+    
+    if(inputValue>0){
+        inputField.style.borderColor = 'none';
+        return inputValue; 
+    }else if(isNaN(inputValue)){
+        inputField.value = '';
+        inputField.placeholder = 'Text Or Empty Not Accepted';
+        inputField.style.borderColor = 'red';
+
+    }else if(inputValue<0){
+        inputField.value = '';
+        inputField.placeholder = 'Nagative Value Not Accepted';
+        inputField.style.borderColor = 'red';
+    }
+}
+
+
 // Get Calculate Button 
 document.getElementById('calculate-btn').addEventListener('click', function(){
     // get total Income 
-    const incomeField = document.getElementById('total-income');
-    const incomeValue = incomeField.value;
-    const totalIncome = parseFloat(incomeValue);
+    // const incomeField = document.getElementById('total-income');
+    // const incomeValue = incomeField.value;
+    const totalIncome = valueToNumber('total-income');
     // console.log(totalIncome);
-    if(totalIncome<0){
-        incomeField.value = '';
-        incomeField.placeholder = 'Nagative Value Not Accepted';
-        incomeField.style.borderColor = 'red';
-    }else if(isNaN(totalIncome)){
-        incomeField.value = '';
-        incomeField.placeholder = 'String Or Empty Not Accepted';
-        incomeField.style.borderColor = 'red';
-    }
+    // if(totalIncome<0){
+    //     incomeField.value = '';
+    //     incomeField.placeholder = 'Nagative Value Not Accepted';
+    //     incomeField.style.borderColor = 'red';
+    // }else if(isNaN(totalIncome)){
+    //     incomeField.value = '';
+    //     incomeField.placeholder = 'String Or Empty Not Accepted';
+    //     incomeField.style.borderColor = 'red';
+    // }
 
 
     // get food cost 
-    const foodCostField = document.getElementById('food-cost');
-    const foodCostValue =  foodCostField.value;
-    const foodCost = parseFloat(foodCostValue);
+    // const foodCostField = document.getElementById('food-cost');
+    // const foodCostValue =  foodCostField.value;
+    // const foodCost = parseFloat(foodCostValue);
 
-    if(foodCost<0){
-        foodCostField.value = 'Enter Positive Number';
-        foodCostField.style.borderColor = 'red';
-    }else if(isNaN(foodCost)){
-        foodCostField.value = 'Text Not Accepted';
-        foodCostField.style.borderColor = 'red';
-    }
+    const foodCost = valueToNumber('food-cost');
+
+    // if(foodCost<0){
+    //     foodCostField.value = 'Enter Positive Number';
+    //     foodCostField.style.borderColor = 'red';
+    // }else if(isNaN(foodCost)){
+    //     foodCostField.value = 'Text Not Accepted';
+    //     foodCostField.style.borderColor = 'red';
+    // }
 
 
 
     // get rent cost value 
-    const rentCostField = document.getElementById('rent-cost');
-    const rentCostValue =  rentCostField.value;
-    const rentCost = parseFloat(rentCostValue);
+    // const rentCostField = document.getElementById('rent-cost');
+    // const rentCostValue =  rentCostField.value;
+    // const rentCost = parseFloat(rentCostValue);
+    const rentCost = valueToNumber('rent-cost');
 
-    if(rentCost<0){
-        rentCostField.value = 'Enter Positive Number';
-        rentCostField.style.borderColor = 'red';
-    }else if(isNaN(rentCost)){
-        rentCostField.value = 'Text or Empty Not Accepted';
-        rentCostField.style.borderColor = 'red';
-    }
+    // if(rentCost<0){
+    //     rentCostField.value = 'Enter Positive Number';
+    //     rentCostField.style.borderColor = 'red';
+    // }else if(isNaN(rentCost)){
+    //     rentCostField.value = 'Text or Empty Not Accepted';
+    //     rentCostField.style.borderColor = 'red';
+    // }
 
     // get clothe cost value 
-    const clotheCostField = document.getElementById('clothe-cost');
-    const clotheCostValue =  clotheCostField.value;
-    const clotheCost = parseFloat(clotheCostValue);
+    // const clotheCostField = document.getElementById('clothe-cost');
+    // const clotheCostValue =  clotheCostField.value;
+    // const clotheCost = parseFloat(clotheCostValue);
+    const clotheCost = valueToNumber('clothe-cost');
 
-    if(clotheCost<0){
-        clotheCostField.value = 'Enter Positive Number';
-        clotheCostField.style.borderColor = 'red';
-    }else if(isNaN(clotheCost)){
-        clotheCostField.value = 'Text Not Accepted';
-        clotheCostField.style.borderColor = 'red';
-    }
+    // if(clotheCost<0){
+    //     clotheCostField.value = 'Enter Positive Number';
+    //     clotheCostField.style.borderColor = 'red';
+    // }else if(isNaN(clotheCost)){
+    //     clotheCostField.value = 'Text Not Accepted';
+    //     clotheCostField.style.borderColor = 'red';
+    // }
 
 
     // total Expenses
